49f5a247659cd67342d6f91115411bd9b7012f4f
bcauchon <bcauchon@gmail.com>
2019-07-26 11:57:41 -0700

    add routes for adding a new word

--- app.rb
+++ app.rb
@@ -13,3 +13,13 @@ get '/words' do
   @words = Word.all
   erb(:words)
 end
+
+get '/words/new' do
+  erb(:new_word)
+end
+
+post '/new_word' do
+  word = Word.new({ :text => params[:word_text] })
+  word.save
+  redirect to '/words'
+end
