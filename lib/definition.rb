7c9496f16cd839911a2aea229df27cb6f0773024
bcauchon <bcauchon@gmail.com>
2019-07-26 09:21:29 -0700

    add Word and Definition class implementations

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,3 +1,53 @@
 class Definition
+  attr_accessor :text
+  attr_reader :id, :word_id
 
+  @@definitions = {}
+  @@next_id = 0
+
+  def initialize(attributes)
+    @text = attributes[:text]
+    assert(attributes[:word_id].class == "Integer", "non-integer word_id")
+    @word_id = attributes[:word_id]
+    @id = attributes[:id] || @next_id += 1
+  end
+
+  def self.all
+    @@definitions.values
+  end
+
+  def ==(other_definition)
+    (@text == other_definition.text) &&
+    (@word_id == other_definition.word_id) &&
+    (@id == other_definition.id)
+  end
+
+  def self.clear
+    @@definitions = {}
+    @@next_id = 0
+  end
+
+  def self.find(id)
+    @@definitions[id.to_i]
+  end
+
+  def self.find_by_word(word_id)
+    @@definitions.values.select { |definition| definition.word_id == word_id }
+  end
+
+  def save
+    @@definitions[@id] = Definition.new({:text => @text, :word_id => @word_id, :id => @id})
+  end
+
+  def update(text)
+    @text = text
+  end
+
+  def delete
+    @@definitions.delete(@id)
+  end
+
+  def word
+    Word.find(@word_id)
+  end
 end
--- lib/word.rb
+++ lib/word.rb
@@ -1,3 +1,46 @@
 class Word
+  attr_accessor :text
+  attr_reader :id
 
+  @@words = {}
+  @@next_id = 0
+
+  def initialize(attributes)
+    @text = attributes[:text]
+    @id = attributes[:id] || @next_id += 1
+  end
+
+  def self.all
+    @@words.values
+  end
+
+  def ==(other_word)
+    (@text == other_word.text) &&
+    (@id == other_word.id)
+  end
+
+  def self.clear
+    @@words = {}
+    @@next_id = 0
+  end
+
+  def self.find(id)
+    @@words[id.to_i]
+  end
+
+  def save
+    @@words[@id] = Word.new({:text => @text, :id => @id})
+  end
+
+  def update(text)
+    @text = text
+  end
+
+  def delete
+    @@words.delete(@id)
+  end
+
+  def definitions
+    Definition.find_by_word(@id)
+  end
 end
