bad844c36e9a6df9d2db18ff290fbb13e008d7e1
bcauchon <bcauchon@gmail.com>
2019-07-26 12:43:04 -0700

    add views and routes for adding a new definition

--- app.rb
+++ app.rb
@@ -23,3 +23,27 @@ post '/new_word' do
   word.save
   redirect to '/words'
 end
+
+get '/words/:id/definitions' do
+  id = params[:id].to_i
+  @word = Word.find(id)
+  @definitions = Definition.find_by_word(id)
+  erb(:definitions)
+end
+
+get '/words/:id/definitions/new' do
+  @word = Word.find(params[:id].to_i)
+  erb(:new_definition)
+end
+
+post '/words/:id/definitions/new_definition' do
+  word = Word.find(params[:id].to_i)
+  definition = Definition.new({
+    :text => params[:definition_text],
+    :word_id => word.id
+  })
+  definition.save
+  # @definitions = Definition.all
+  # erb(:definitions)
+  redirect to "/words/#{word.id}/definitions"
+end
